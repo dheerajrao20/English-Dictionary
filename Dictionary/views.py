fed287370c1891c76affd04bbca5b7122869be68
DHEERAJ KUMAR <dheerajrao20@outlook.com>
2022-05-02 19:58:30 +0530

    finalise

--- Dictionary/views.py
+++ Dictionary/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from PyDictionary import PyDictionary
 
 # Create your views here.
-
 def home(request):
     return render(request, 'index.html')
 
-
 def word(request):
-    return render(request, 'word.html')
+    search = request.GET.get('search')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+    context = {
+        'meaning': meaning,
+    }
+    return render(request, 'word.html', context)
